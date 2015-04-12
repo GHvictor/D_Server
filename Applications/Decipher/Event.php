278170ef4bfb61ba88661cf6d678722b1962c6e8
GHvictor <victor.vrain@gmail.com>
2015-04-12 20:53:09 +0800

    注册

--- Applications/Decipher/Event.php
+++ Applications/Decipher/Event.php
@@ -39,21 +39,31 @@ class Event
 		$client_name = $message_data['account'];
 		$client_pass = $message_data['password']; 
 
-                if($client_pass == $db->single("select password from UserInf where userName = '$client_name' ")){
+                if($client_pass == $db->single("select password from UserInf
+                                                where userName = '$client_name' ")){
                         echo "登录";
 			$re_client_id = $client_id + 1;
-                 	$row_cout = $db->query("update Id_Name set clientId = '$re_client_id' where clientName = '$client_name' ");
-			Gateway::sendToCurrentClient('{"type":"back","re_message":"true"}');
+                 	$row_cout = $db->query("update Id_Name set clientId = '$re_client_id'
+                                                where clientName = '$client_name' ");
+			Gateway::sendToCurrentClient('{"re_type":"0","re_message":"true"}');
 		}
                 else{
-			Gateway::sendToCurrentClient('{"type":"back","re_message":"false"}');
+			Gateway::sendToCurrentClient('{"re_type":"0","re_message":"false"}');
 			echo "失败";
 		}
 	    break;
-            // 聊天
             case '1':
+		
+		$insert_User = $db->query("insert into UserInf
+                              (password,userName,birth,gender,avatarId,isOnline,gameInf)
+                              values ('','','','','','','')");
+		$insert_Id = $db->query("insert into Id_Name
+					 (clientId,clientName) values (-1,'')");
+            // 聊天
+            case '2':
 		$reClientName = $message_data['re_account'];
-		$reClientId = $db->single("select clientId from Id_Name where clientName = '$reClientName' ");
+		$reClientId = $db->single("select clientId from Id_Name
+                                           where clientName = '$reClientName' ");
 	
 	        return Gateway::sendToClient($reClientId,$message_data['message']); 
         }
@@ -71,7 +81,8 @@ class Event
        
        echo $client_name;
        $db = Db::instance('DecipherDb');
-       $row_cout = $db->query("update Id_Name set clientId = -1 where clientName = '$client_name' ");	
+       $row_cout = $db->query("update Id_Name set clientId = -1
+                               where clientName = '$client_name' ");	
        GateWay::sendToAll(json_encode(array('type'=>'closed', 'id'=>$client_id)));
    }
 }
